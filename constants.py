97e81245d69cf97e716f0520e2742002f8efd952
Roman Potekhin <rpotekhin@ivi.ru>
2015-05-25 20:14:06 +0400

    WS chat realization

--- constants.py
+++ constants.py
@@ -5,10 +5,20 @@ CMD_CHANGENICK = "changenick"
 CMD_JOINROOM = "joinroom"
 CMD_LEAVEROOM = "leaveroom"
 CMD_MESSAGE = "message"
+CMD_LOGIN = "login"
 ALLOWED_COMMANDS = (CMD_CREATEROOM,
                     CMD_GETROOMLIST,
                     CMD_GETUSERLIST,
                     CMD_CHANGENICK,
                     CMD_JOINROOM,
                     CMD_LEAVEROOM,
-                    CMD_MESSAGE)
+                    CMD_MESSAGE,
+                    CMD_LOGIN)
+EVT_ROOMLIST = "roomlist"
+EVT_USERLIST = "userlist"
+EVT_ROOMCREATED = "roomcreated"
+EVT_CHANGEDNICK = "changednick"
+EVT_JOINROOM = "joinroom"
+EVT_LEAVEROOM = "leaveroom"
+EVT_MESSAGE = "message"
+EVT_LOGIN = "login"
--- server.py
+++ server.py
@@ -1,9 +1,7 @@
 #! /usr/bin/python
 # -*- coding: utf-8  -*-
-import random
-import string
 import json
-from collections import defaultdict
+from uuid import uuid4
 
 
 from tornado import ioloop
@@ -14,37 +12,28 @@ from tornado.web import RequestHandler
 
 from constants import ALLOWED_COMMANDS
 from constants import CMD_CREATEROOM, CMD_GETROOMLIST, CMD_GETUSERLIST
-from constants import CMD_CHANGENICK, CMD_JOINROOM, CMD_LEAVEROOM, CMD_MESSAGE
-
-RANDOM_USERNAME_LENGTH = 10
-
-
-def generate_random_username():
-    return "".join(random.choice(string.letters) for i in range(RANDOM_USERNAME_LENGTH))
-
-
-class UserAlreadyExistsError(Exception):
-    pass
+from constants import CMD_CHANGENICK, CMD_JOINROOM, CMD_LEAVEROOM
+from constants import CMD_MESSAGE, CMD_LOGIN
+from constants import EVT_ROOMLIST, EVT_USERLIST, EVT_CHANGEDNICK
+from constants import EVT_JOINROOM, EVT_LEAVEROOM, EVT_MESSAGE
+from constants import EVT_LOGIN, EVT_ROOMCREATED
 
 
 class Room(object):
     def __init__(self, user_handlers, name, users=None):
         if users is None:
             self.users = set()
-        self.users = users
+        else:
+            self.users = users
         self.name = name
         self.user_handlers = user_handlers
 
-    @property
-    def users(self):
-        return self.users
-
-    @property
-    def name(self):
-        return self.name
+    def change_nick(self, from_nick, to_nick):
+        self.users.discard(from_nick)
+        self.users.add(to_nick)
 
     def broadcast(self, msg):
-        for user in users:
+        for user in self.users:
             try:
                 handler = self.user_handlers[user]
             except KeyError:
@@ -53,102 +42,152 @@ class Room(object):
                 handler.write_message(msg)
 
     def join(self, username):
-        if username in users:
-            raise UserAlreadyExistsError
+        if username in self.users:
+            return
         self.users.add(username)
-        self.broadcast({"event": "joinroom",
+        self.broadcast({"event": EVT_JOINROOM,
                         "username": username,
                         "room": self.name})
 
     def leave(self, username):
-        if username not in users:
+        if username not in self.users:
             return
-        self.users.remove(username)
-        self.broadcast({"event": "leaveroom",
+        self.broadcast({"event": EVT_LEAVEROOM,
                         "username": username,
                         "room": self.name})
+        self.users.discard(username)
 
     def message(self, username, message):
-        if username not in users:
+        if username not in self.users:
             return
-        self.broadcast({"event": "message",
+        self.broadcast({"event": EVT_MESSAGE,
                         "username": username,
                         "message": message})
 
 
 class WebSocketChat(WebSocketHandler):
-    def initialize(self, users, rooms, users2rooms):
+    def initialize(self, users, rooms, user2room, loggedin_users):
         self.users = users
         self.rooms = rooms
-        self.users2rooms = users2rooms
+        self.user2room = user2room
+        self.loggedin_users = loggedin_users
 
     def check_origin(self, origin):
         return True
 
     def open(self):
-        self.username = generate_random_username()
+        self.username = str(uuid4())
         self.users[self.username] = self
 
     def write_error(self, error_msg):
         self.write_message({"error": error_msg})
+
+    def broadcast_roomlist(self):
+        msg = {"event": EVT_ROOMLIST, "rooms": list(self.rooms)}
+        self.broadcast_all(msg)
+
+    def broadcast_userlist(self):
+        msg = {"event": EVT_USERLIST, "users": list(self.loggedin_users)}
+        self.broadcast_all(msg)
 
     def on_message(self, message):
         try:
             msg = json.loads(message)
         except ValueError:
             return
-        if ("cmd" not in msg) or ("args" not in msg):
+        if ("cmd" not in msg):
+            self.write_error("Incorrect command")
             return
         cmd = msg["cmd"]
         if cmd not in ALLOWED_COMMANDS:
             self.write_error("Incorrect command")
-        if cmd == CMD_CREATEROOM:
+            return
+        if cmd not in (CMD_LOGIN, CMD_GETROOMLIST, CMD_GETUSERLIST) \
+                and self.username not in self.loggedin_users:
+            self.write_error("You must be logged in before execute commands")
+            return
+        if cmd == CMD_LOGIN:
+            nick = msg["args"]["nick"]
+            if self.username in self.loggedin_users:
+                self.write_error("You had already logged in")
+                return
+            if nick in self.users:
+                self.write_error("This nick already used")
+                return
+            else:
+                del self.users[self.username]
+                self.username = nick
+                self.loggedin_users.add(self.username)
+                self.users[self.username] = self
+                self.broadcast_all({"event": EVT_LOGIN, "nick": nick})
+                self.broadcast_userlist()
+        elif cmd == CMD_CREATEROOM:
             room_name = msg["args"]["room_name"]
             if room_name in self.rooms:
                 self.write_error("Room already exists")
             else:
                 self.rooms[room_name] = Room(self.users, room_name)
-                msg = {"event": "roomcreated", "name": room_name}
+                msg = {"event": EVT_ROOMCREATED, "name": room_name}
                 self.broadcast_all(msg)
+                self.broadcast_roomlist()
         elif cmd == CMD_GETROOMLIST:
-            msg = {"event": "roomlist", "rooms": list(self.rooms)}
+            msg = {"event": EVT_ROOMLIST, "rooms": list(self.rooms)}
             self.write_message(msg)
         elif cmd == CMD_GETUSERLIST:
-            msg = {"event": "userlist", "users": list(self.users)}
+            msg = {"event": EVT_USERLIST, "users": list(self.loggedin_users)}
             self.write_message(msg)
         elif cmd == CMD_CHANGENICK:
-            to_nick = cmd["args"]["change_to"]
+            to_nick = msg["args"]["change_to"]
             if to_nick in self.users:
                 self.write_error("This nick already used")
             else:
-                msg = {"event": "changed_nick",
-                       "from": self.username,
+                from_nick = self.username
+                self.loggedin_users.discard(from_nick)
+                self.loggedin_users.add(to_nick)
+                self.users[to_nick] = self.users[self.username]
+                if self.username in self.user2room:
+                    current_room = self.user2room[self.username]
+                    self.user2room[to_nick] = self.user2room[self.username]
+                    self.rooms[current_room].change_nick(from_nick, to_nick)
+                    del self.user2room[self.username]
+                del self.users[self.username]
+                self.username = to_nick
+                msg = {"event": EVT_CHANGEDNICK,
+                       "from": from_nick,
                        "to": to_nick}
                 self.broadcast_all(msg)
-        elif cmd == CMD_JOINROOM or cmd == CMD_LEAVEROOM:
-            room_name = cmd["args"]["name"]
+                self.broadcast_userlist()
+        elif cmd in (CMD_JOINROOM, CMD_LEAVEROOM):
+            room_name = msg["args"]["name"]
             if room_name not in self.rooms:
                 self.write_error("Operation with nonexistent room")
             else:
                 if cmd == CMD_JOINROOM:
+                    if self.username in self.user2room:
+                        current_room = self.user2room[self.username]
+                        self.rooms[current_room].leave(self.username)
                     self.rooms[room_name].join(self.username)
-                    self.users2rooms[self.username].add(room_name)
+                    self.user2room[self.username] = room_name
                 else:
-                    self.rooms[room_name].leave(self.username)
-                    self.users2rooms[self.username].remove(room_name)
+                    if self.username in self.user2room:
+                        self.rooms[room_name].leave(self.username)
+                        current_room = self.user2room[self.username]
+                        if current_room == room_name:
+                            del self.user2room[self.username]
         elif cmd == CMD_MESSAGE:
-            msg = cmd["args"]["msg"]
-            for room_name in self.users2rooms[self.username]:
-                self.rooms[room_name].message(self.username, msg)
+            msg = msg["args"]["msg"]
+            if self.username in self.user2room:
+                current_room = self.user2room[self.username]
+                self.rooms[current_room].message(self.username, msg)
 
     def broadcast_all(self, msg):
         for handler in self.users.values():
             handler.write_message(msg)
 
     def on_close(self):
-        for room in self.rooms:
-            room.leave(self.username)
         del self.users[self.username]
+        self.loggedin_users.discard(self.username)
+        self.broadcast_userlist()
 
 
 class IndexPage(RequestHandler):
@@ -159,9 +198,14 @@ class IndexPage(RequestHandler):
 if __name__ == '__main__':
     users = {}
     rooms = {}
-    users2rooms = defaultdict(set)
+    user2room = {}
+    loggedin_users = set()
     application = Application([
-        (r'/websocket', WebSocketChat, dict(users=users, rooms=rooms, users2rooms=users2rooms)),
+        (r'/websocket', WebSocketChat,
+                        dict(users=users,
+                             rooms=rooms,
+                             user2room=user2room,
+                             loggedin_users=loggedin_users)),
         (r'/', IndexPage)
     ])
     application.listen(8888)
